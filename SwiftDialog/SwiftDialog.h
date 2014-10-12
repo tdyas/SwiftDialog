9514ff9046fe9076a537da3d86114f5b2257f9e1
Tom Dyas <tom@shoalsoftware.com>
2014-10-12 11:51:26 -0400

    Initial Commit

---
+++ SwiftDialog/SwiftDialog.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDialog.h
+//  SwiftDialog
+//
+//  Created by Tom Dyas on 10/12/14.
+//  Copyright (c) 2014 Shoal Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDialog.
+FOUNDATION_EXPORT double SwiftDialogVersionNumber;
+
+//! Project version string for SwiftDialog.
+FOUNDATION_EXPORT const unsigned char SwiftDialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDialog/PublicHeader.h>
+
+
