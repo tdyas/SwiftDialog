08681ee6d96ea340b226e4ad2a26e83b3b0605b1
Tom Dyas <tom@shoalsoftware.com>
2014-10-19 17:40:50 -0400

    move BoolElement to its own file

---
+++ SwiftDialog/BoolElement.swift
@@ -0,0 +1,58 @@
+// Copyright 2014 Thomas K. Dyas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+
+public class BoolElement : Element {
+    private var switchControl: UISwitch!
+    
+    public var caption: String = ""
+    public var value: Bool = false
+    
+    public init(caption: String = "", value: Bool = false) {
+        super.init()
+        self.caption = caption
+        self.value = value
+    }
+    
+    func valueChanged() {
+        self.value = self.switchControl.on
+    }
+    
+    public override func getCell(tableView: UITableView) -> UITableViewCell! {
+        let cellKey = "bool"
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellKey) as UITableViewCell!
+        if cell == nil {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: cellKey)
+            cell.selectionStyle = .None
+        }
+        
+        if self.switchControl == nil {
+            self.switchControl = UISwitch()
+            self.switchControl.backgroundColor = UIColor.clearColor()
+            self.switchControl.addTarget(self, action: "valueChanged", forControlEvents: .ValueChanged)
+        }
+        
+        if !self.switchControl.isDescendantOfView(cell)  {
+            switchControl.removeFromSuperview()
+        }
+        cell.accessoryView = switchControl
+        
+        cell.textLabel?.text = self.caption
+        self.switchControl.on = self.value
+        
+        return cell
+    }
+}
