11dcbf33c6661cfd5e8092c5ccf96eddc71dcd28
Tom Dyas <tom@shoalsoftware.com>
2014-12-14 23:14:26 -0500

    add missing import statements

--- SwiftDialog/RootElement.swift
+++ SwiftDialog/RootElement.swift
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import UIKit
+
 @objc
 protocol BooleanValuedElement {
     var value: Bool { get }
--- SwiftDialog/SectionElement.swift
+++ SwiftDialog/SectionElement.swift
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import UIKit
+
 public class SectionElement : Element {
     public var elements: [Element]
     public var header: String?
